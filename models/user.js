7af70ad29f19f0c5b0fb8336b1dc12753becebfc
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2023-02-08 17:14:56 -0800

    Create connection file, server file, package.json, and User model. Create MVC file structure.

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please add a valid email'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  })
+  .set(function (v) {
+    this.set({ v });
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
